cf34fb5ea33b6d535bba06d82a4ce6649fb06636
kdh92417 <kdh92417@gmail.com>
2022-06-23 00:24:33 +0900

    feat: user 생성 로직 보완 및 repository 추가

--- app/application/service/user.py
+++ app/application/service/user.py
@@ -1,8 +1,16 @@
+from app.application.interfaces.user_repository import AbstractRepository
 from app.domain.entity import User
 
 
-# 클라이언트가 무엇을 해줄지, 유즈케이스부터 작성
-# 어떤도구 어떤 프레임워크를 사용할지는 나중에
-def create_user(user_name: str):
-    user = User(name=user_name)
-    return user
+class UserService:
+    def __init__(self, repository: AbstractRepository):
+        self.repository = repository
+
+    # 클라이언트가 무엇을 해줄지, 유즈케이스부터 작성
+    # 어떤도구 어떤 프레임워크를 사용할지는 나중에
+    def create_user(self, user_name: str):
+        # 데이터베이스에 저장
+        _user = User(name=user_name)
+        user = self.repository.create(_user)
+
+        return user
